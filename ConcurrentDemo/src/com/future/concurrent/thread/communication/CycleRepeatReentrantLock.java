f3f755e4a76809030573072ab951da31abc3e915
zhoujie277 <463359476@qq.com>
2015-02-09 18:37:22 +0800

    完善线程通信的demo

--- ConcurrentDemo/src/com/future/concurrent/thread/communication/CycleRepeatReentrantLock.java
+++ ConcurrentDemo/src/com/future/concurrent/thread/communication/CycleRepeatReentrantLock.java
@@ -3,22 +3,27 @@
  */
 package com.future.concurrent.thread.communication;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 子线程循环10次，接着主线程循环100，接着又回到子线程循环10次，接着再回到主线程又循环100，如此循环50次，请写出程序。
  * 该程序的缺点：
- * 1. 用synchronized，没有区分读写锁，性能不佳. 或者暴露了isSubRunning，让其在多线程环境下访问.(注释)
+ * 1. 用统一的锁与Synchronized一样，没有区分读写锁，性能不佳. 或者暴露了isSubRunning，让其在多线程环境下访问.(注释)
  * 2. 每个循环都新开了一个线程，浪费资源(考虑用阻塞队列)
- * 3. for循环没有包括在锁内，虽然在本程序不会出错，但是封装的不够好，出现在多线程程序中容易出错.
  * @author JayZhou
  * @date: 2015年2月9日 
  */
-public class CycleRepeatSynchronized {
+public class CycleRepeatReentrantLock {
 
-    private Object objLock = new Object();
     private volatile boolean isSubRunning = true;
 
+    private Lock lock = new ReentrantLock();
+    private Condition _condition = lock.newCondition();
+
     public static void main(String[] args) {
-        CycleRepeatSynchronized demo = new CycleRepeatSynchronized();
+        CycleRepeatReentrantLock demo = new CycleRepeatReentrantLock();
         SubThread subThread = demo.new SubThread();
         for (int i = 0; i < 50; i++) {
             new Thread(subThread).start();
@@ -31,24 +36,23 @@ public class CycleRepeatSynchronized {
      */
     public void cycleHundred() {
         // 缺点：每次进入都会被锁住
-        // if(isSubRunning){
-        synchronized (objLock) {
+        try {
+            lock.lock();
             while (isSubRunning) {
                 try {
-                    objLock.wait();
+                    _condition.await();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        }
-        // }
-        for (int i = 0; i < 100; i++) {
-            System.out.print("cycleHundred: " + i + " ");
-        }
-        System.out.println();
-        synchronized (objLock) {
+            for (int i = 0; i < 100; i++) {
+                System.out.print("cycleHundred: " + i + " ");
+            }
+            System.out.println();
             isSubRunning = true;
-            objLock.notifyAll();
+            _condition.signalAll();
+        } finally {
+            lock.unlock();
         }
     }
 
@@ -57,24 +61,23 @@ public class CycleRepeatSynchronized {
      */
     public void cycleTen() {
         // 缺点：每次进入都会被锁住
-        // if(!isSubRunning){
-        synchronized (objLock) {
+        try {
+            lock.lock();
             while (!isSubRunning) {
                 try {
-                    objLock.wait();
+                    _condition.await();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        }
-        // }
-        for (int i = 0; i < 10; i++) {
-            System.out.print("cycleTen: " + i + " ");
-        }
-        System.out.println();
-        synchronized (objLock) {
+            for (int i = 0; i < 10; i++) {
+                System.out.print("cycleTen: " + i + " ");
+            }
+            System.out.println();
             isSubRunning = false;
-            objLock.notifyAll();
+            _condition.signalAll();
+        } finally {
+            lock.unlock();
         }
     }
 
